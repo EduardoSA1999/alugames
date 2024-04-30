c306c9feba62637af3ec589013c3a499fb569dd7
EduardoSA1999 <eduardoamaral1914@gmail.com>
2024-04-30 11:50:27 -0300

    Contar e exibir jogos alugados

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+let jogosAlugados = 0
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}.`);
+}
+
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
@@ -9,10 +15,20 @@ function alterarStatus(id) {
            imagem.classList.remove('dashboard__item__img--rented');
            botao.classList.remove('dashboard__item__button--return');
            botao.textContent = 'Alugar';
+           jogosAlugados--;
         }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
+        jogosAlugados++
     }
+
+    contarEExibirJogosAlugados();
+
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
